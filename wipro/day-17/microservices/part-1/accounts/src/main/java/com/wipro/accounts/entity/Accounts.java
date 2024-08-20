41110a4ee1671b2b0ce69020be9cfde4b8711551
suniledureka <sunil.edureka@gmail.com>
2024-08-20 14:33:43 +0530

    day-17

---
+++ wipro/day-17/microservices/part-1/accounts/src/main/java/com/wipro/accounts/entity/Accounts.java
@@ -0,0 +1,26 @@
+package com.wipro.accounts.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter 
+@Setter 
+@ToString 
+@AllArgsConstructor 
+@NoArgsConstructor
+public class Accounts extends  BaseEntity {
+	
+    @Id
+    @Column(name="account_number")
+    private Long accountNumber;
+    
+    @Column(name="customer_id")
+    private Long customerId;
+
+    @Column(name="account_type")
+    private String accountType;
+
+    @Column(name="branch_address")
+    private String branchAddress;
+}
