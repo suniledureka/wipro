13da184087dc2e820520e502d1f66ed4f798e224
suniledureka <sunil.edureka@gmail.com>
2025-01-25 14:17:16 +0530

    deleted

--- java/day-2/codes/Bank.java
+++
@@ -1,9 +0,0 @@
-interface Bank
-{
-	int n = 25; //public static final
-
-	void deposit(float amt); //public abstract
-	//default void withdraw(float amt){}
-	
-	void withdraw(float amt); //public abstract
-}
--- java/day-2/codes/SBI.java
+++
@@ -1,9 +0,0 @@
-class SBI implements Bank 
-{
-	public void deposit(float amt){
-		System.out.println("Rs." + amt + " Credited to SBI Account");
-	}
-	public void withdraw(float amt){
-		System.out.println("Rs." + amt + " Debited from SBI Account");
-	}
-}
