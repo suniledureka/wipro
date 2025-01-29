4ab39bc6b957b03e09992a014ae358b5c42e7b4d
suniledureka <sunil.edureka@gmail.com>
2025-01-29 11:34:02 +0530

    day-7

--- java/day-6/codes/05.junit-mocking/src/main/java/com/wipro/ems/dao/EmployeeDAO.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.ems.dao;
-
-import java.util.List;
-
-public interface EmployeeDAO {
-	public String findEmployeeNameById(int eid);
-	
-	public List<String> findEmployeeNames();
-}
--- java/day-6/codes/05.junit-mocking/src/main/java/com/wipro/ems/services/EmployeeServices.java
+++
@@ -1,9 +0,0 @@
-package com.wipro.ems.services;
-
-import java.util.List;
-
-public interface EmployeeServices {
-	public String getEmployeeNameById(int eid);
-	
-	public List<String> findAllNames();
-}
