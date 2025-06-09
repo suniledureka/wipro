382a12116432df96a57547b1777afba9620c5af9
suniledureka <sunil.edureka@gmail.com>
2025-06-09 15:36:29 +0530

    day-24

---
+++ day-24/06.react-http-fetch/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ProductsList from './components/ProductsList';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductsList />
+    </div>
+  );
+}
+
+export default App;
