7c0089b216b790a5c2422a412b93f97e3a2fd2b2
suniledureka <sunil.edureka@gmail.com>
2025-06-14 14:48:44 +0530

    day-28

--- day-28/04.react-rtl-cakeshop/src/store/store.js
+++
@@ -1,7 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import cakeReducer from '../features/cake/cakeSlice'
-
-const store = configureStore({
-            reducer: cakeReducer
-        })
-export default store;        
---
+++ day-29/back-end/Controllers/AuthController.js
@@ -0,0 +1,74 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const UserModel = require("../Models/User");
+
+
+const signup = async (req, res) => {
+    console.log('inside signup',req.body)
+    try {
+        const { name, email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        if (user) {
+            return res.status(409)
+                .json({ message: 'User Already Exists!!!', success: false });
+        }
+        const userModel = new UserModel({ name, email, password });
+        userModel.password = await bcrypt.hash(password, 10);
+        await userModel.save();
+        res.status(201)
+            .json({
+                message: "User Created.. Try Login",
+                success: true
+            })
+    } catch (err) {
+        console.log(err)
+        res.status(500)
+            .json({
+                message: "Internal server errror",
+                success: false
+            })
+    }
+}
+
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await UserModel.findOne({ email });
+        const errorMsg = 'Authentication Failed.. No User exists with UserName/ Password';
+        if (!user) {
+            return res.status(401)
+                .json({ message: errorMsg, success: false });
+        }
+        const isPassEqual = await bcrypt.compare(password, user.password);
+        if (!isPassEqual) {
+            return res.status(401)
+                .json({ message: errorMsg, success: false });
+        }
+
+        const jwtToken = jwt.sign(
+            { email: user.email, _id: user._id },
+            process.env.JWT_SECRET,            
+            { algorithm: 'HS256', expiresIn: '24h' }
+        )
+
+        res.status(200)
+            .json({
+                message: "Login Success",
+                success: true,
+                jwtToken,
+                email,
+                name: user.name
+            })
+    } catch (err) {
+        res.status(500)
+            .json({
+                message: "Internal server errror",
+                success: false
+            })
+    }
+}
+
+module.exports = {
+    signup, 
+    login
+}
